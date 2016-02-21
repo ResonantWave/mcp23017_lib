e0b18ec32d43244d7c40a0a1eebcf1f1b377218b
nixijav <nixijav@nixijav.com>
2016-02-21 21:27:40 +0100

    Added pullup and finished documentation. Removed hardcoded i2c address

--- README.md
+++ README.md
@@ -13,15 +13,35 @@ $ sudo apt-get install python-smbus i2c-tools
 
 This library aims at being very similar to the RPi.GPIO library. At the moment it provides the following functions:
 
-`setup(pin, mode)`: Writes direction (IN, OUT) to the specified pin.
+`start(address)`: Starts the library on the specified chip address.
+`setup(pin, mode, pullupEnable)`: Writes direction (IN, OUT) to the specified pin. `pullupEnable` enables the internal 100k pullup resistor. Optional, and only works when setting a pin as an input. Valid values are PUHIGH and PULOW.
 `output(pin, dir)`: Writes state (HIGH, LOW) to the specified pin.
 `input(pin)`: Reads and returns pin state (True, False)
+
+`puRead(bank)`: Reads and returns the pullup registers from both GPIO banks.
+   - Valid parameter values: **PU_A**, **PU_B**, **ALL** or leave blank for a default graphic diagram of both banks.
 `dirRead(bank)`: Reads and returns the direction registers from both GPIO banks.
-   - Valid parameter values: BANK_A, BANK_B, ALL or leave blank for a default graphic diagram of both banks.
+   - Valid parameter values: **BANK_A**, **BANK_B**, **ALL** or leave blank for a default graphic diagram of both banks.
 `latRead(bank)`: Reads and returns the latch registers from both GPIO banks.
-   - Valid parameter values: LAT_A, LAT_B, ALL or leave blank for a default graphic diagram of both banks.
+   - Valid parameter values: **LAT_A**, **LAT_B**, **ALL** or leave blank for a default graphic diagram of both banks.
 
 Please refer to the image below for the pin mapping.
+
+### Constant list
+
+ * MCP.OUT = 1
+ * MCP.IN = 0
+ * MCP.BANK_A = 0
+ * MCP.BANK_B = 1
+ * MCP.ALL = 2
+ * MCP.LAT_A = 3
+ * MCP.LAT_B = 4
+ * MCP.PU_A = 5
+ * MCP.PU_B = 6
+ * MCP.HIGH = 1
+ * MCP.LOW = 0
+ * MCP.PUHIGH = 1
+ * MCP.PULOW = 0
 
 ### Pinout
 
@@ -31,13 +51,15 @@ Pins are mapped according to this diagram:
 
 ### Code example
 
-This example will set pin #2 as an input and #1 as an output.
+This example will set pin #2 as an input, with the pullup resistor enabled and #1 as an output.
 When the input goes `high`, pin #1 will also go high.
 
 ```py
 import mcp23017_lib as MCP
 
-MCP.setup(2, MCP.IN)
+MCP.start(0x26)
+
+MCP.setup(2, MCP.IN, MCP.PUHIGH)
 MCP.setup(1, MCP.OUT)
 
 while 1:
@@ -49,10 +71,10 @@ while 1:
 
 ### TODO list
 
- * Do not use hardcoded i2c address
- * Add raw write function
- * Add pullup function
- * Finish function documentation
+ - [x] ~~Do not use hardcoded i2c address~~
+ - [x] ~~Add pullup function~~
+ - [ ] Add raw write function
+ - [x] Finish function documentation
 
 ### Contributors
 
--- mcp23017_lib.py
+++ mcp23017_lib.py
@@ -8,32 +8,57 @@ import smbus
 
 i2c = smbus.SMBus(1)
 
-DIRA = str(bin(i2c.read_byte_data(0x26, 0x00))).replace("0b", "").rjust(8, '0')
-DIRB = str(bin(i2c.read_byte_data(0x26, 0x01))).replace("0b", "").rjust(8, '0')
-
-LATA = str(bin(i2c.read_byte_data(0x26, 0x14))).replace("0b", "").rjust(8, '0')
-LATB = str(bin(i2c.read_byte_data(0x26, 0x15))).replace("0b", "").rjust(8, '0')
-
 OUT = 0
 IN = 1
+
 BANK_A = 0
 BANK_B = 1
 ALL = 2
 LAT_A = 3
 LAT_B = 4
+PU_A = 5
+PU_B = 6
+
 HIGH = 1
 LOW = 0
+
+PUHIGH = 1
+PULOW = 0
+
+ADDRESS = 0x20
+def puRead(bank=-1):
+   """
+   Reads and returns the pullup registers from both GPIO banks
+
+      Param 'bank': select which bank/s should be returned. Use PU_A, PU_B, ALL or leave blank
+      for a default graphic diagram.
+      Return: returns the selected bank/s. If blank, a graphic diagram will be returned instead.
+   """
+   if(bank == PU_A):
+      print PUA
+   elif(bank == PU_B):
+      print PUB
+   elif(bank == ALL):
+      print PUA, PUB
+   else:
+      print " --------"
+      print PUB[7] + "|8    9|" + PUA[0]
+      print PUB[6] + "|7   10|" + PUA[1]
+      print PUB[5] + "|6   11|" + PUA[2]
+      print PUB[4] + "|5   12|" + PUA[3]
+      print PUB[3] + "|4   13|" + PUA[4]
+      print PUB[2] + "|3   14|" + PUA[5]
+      print PUB[1] + "|2   15|" + PUA[6]
+      print PUB[0] + "|1   16|" + PUA[7]
+      print " --------"
 
 def dirRead(bank=-1):
    """
    Reads and returns the direction registers from both GPIO banks
    
-   - **parameters**, **types** and **return**::
-   
-      :param bank: select which bank/s should be returned. Use BANK_A, BANK_B, ALL or leave blank
+      Param 'bank': select which bank/s should be returned. Use BANK_A, BANK_B, ALL or leave blank
       for a default graphic diagram.
-      :type bank: Integer.
-      :return: returns the selected bank/s. If blank, a graphic diagram will be returned instead.
+      Return: returns the selected bank/s. If blank, a graphic diagram will be returned instead.
    """
    if(bank == BANK_A):
       print DIRA
@@ -57,12 +82,9 @@ def latRead(bank=-1):
    """
    Reads and returns the latch registers from both GPIO banks
 
-   - **parameters**, **types** and **return**::
-
-      :param bank: select which latch/es should be returned. Use LAT_A, LAT_B, ALL or leave blank
+      Param 'bank': select which latch/es should be returned. Use LAT_A, LAT_B, ALL or leave blank
       for a default graphic diagram.
-      :type bank: Integer.
-      :return: returns the selected latch/es. If blank, a graphic diagram will be returned instead.
+      Return: returns the selected latch/es. If blank, a graphic diagram will be returned instead.
    """
    if(bank == LAT_A):
       print LATA
@@ -82,19 +104,53 @@ def latRead(bank=-1):
       print LATB[0] + "|1   16|" + LATA[7]
       print " --------"
 
-def setup(pin, mode):
+def start(address):
    """
-   Writes direction (IN, OUT) based on pin number
+   Starts, initializes and reads the chip specified with the address parameter.
+
+      Param 'address': a valid chip address, in hexadecimal format.
+      Return: ValueError if address is invalid
    """
-   global DIRA, DIRB
+
+   global ADDRESS, DIRA, DIRB, LATA, LATB, PUA, PUB
+   try:
+      int(str(address), 16)
+   except:
+      raise ValueError(str(address) + " is not a valid chip address")
+   ADDRESS = address
+   DIRA = str(bin(i2c.read_byte_data(ADDRESS, 0x00))).replace("0b", "").rjust(8, '0')
+   DIRB = str(bin(i2c.read_byte_data(ADDRESS, 0x01))).replace("0b", "").rjust(8, '0')
+
+   LATA = str(bin(i2c.read_byte_data(ADDRESS, 0x14))).replace("0b", "").rjust(8, '0')
+   LATB = str(bin(i2c.read_byte_data(ADDRESS, 0x15))).replace("0b", "").rjust(8, '0')
+
+   PUA = str(bin(i2c.read_byte_data(ADDRESS, 0x0C))).replace("0b", "").rjust(8, '0')
+   PUB = str(bin(i2c.read_byte_data(ADDRESS, 0x0D))).replace("0b", "").rjust(8, '0')
+def setup(pin, mode, pullEnable=None):
+   """
+   Writes direction (IN, OUT) based on pin number. Also enables internall pullup if specified.
+   
+      Param 'pin': Any pin from 1 to 16.
+      Param 'mode': Defines pin direction, input (IN) or output (OUT).
+      Param 'pullEnable' (optional): Enables internal pullup resistor when setting a pin as an input. 
+      Values are PUHIGH and PULOW.
+      Return: ValueError if pin or mode are invalid.
+   """
+   global DIRA, DIRB, PUA, PUB
    if(str(pin).isdigit() and (0 < pin < 17)):
       if(mode == OUT or mode == IN):
          if(0 < pin < 9):
             DIRB = DIRB[0:pin - 1] + str(mode) + DIRB[pin:]
-            i2c.write_byte_data(0x26, 0x01, int(DIRB, 2))
+            i2c.write_byte_data(ADDRESS, 0x01, int(DIRB, 2))
+            if((pullEnable == PUHIGH or pullEnable == PULOW) and mode == IN):
+               PUB = PUB[0:pin - 1] + str(pullEnable) + PUB[pin:]
+               i2c.write_byte_data(ADDRESS, 0x0D, int(PUB, 2))
          elif(8 < pin < 17):
             DIRA = DIRA[0:(pin - 8) - 1] + str(mode) + DIRA[(pin - 8):]
-            i2c.write_byte_data(0x26, 0x00, int(DIRA, 2))
+            i2c.write_byte_data(ADDRESS, 0x00, int(DIRA, 2))
+            if((pullEnable == PUHIGH or pullEnable == PULOW) and mode == IN):
+               PUA = PUA[0:(pin - 8) - 1] + str(pullEnable) + PUA[(pin - 8):]
+               i2c.write_byte_data(ADDRESS, 0x0C, int(PUA, 2))
       else:
          raise ValueError(str(mode) + " is not a valid mode")
    else:
@@ -102,17 +158,21 @@ def setup(pin, mode):
 
 def output(pin, dir):
    """
-   Writes state (HIGH, LOW) based on pin number
+   Writes state (HIGH, LOW) based on pin number.
+
+      Param 'pin': Any pin from 1 to 16.
+      Param 'dir': Defines pin state, on (HIGH) or off (LOW).
+      Return: ValueError if pin or dir are invalid.
    """
    global LATA, LATB
    if(str(pin).isdigit() and (0 < pin < 17)):
       if(dir == HIGH or dir == LOW):
          if(0 < pin < 9):
             LATB = LATB[0:pin - 1] + str(dir) + LATB[pin:]
-            i2c.write_byte_data(0x26, 0x15, int(LATB, 2))
+            i2c.write_byte_data(ADDRESS, 0x15, int(LATB, 2))
          elif(8 < pin < 17):
             LATA = LATA[0:(pin - 8) - 1] + str(dir) + LATA[(pin - 8):]
-            i2c.write_byte_data(0x26, 0x14, int(LATA, 2))
+            i2c.write_byte_data(ADDRESS, 0x14, int(LATA, 2))
       else:
          raise ValueError(str(dir) + " is not a valid state")
    else:
@@ -120,10 +180,13 @@ def output(pin, dir):
 def input(pin):
    """
    Reads and returns pin state.
+   
+      Param 'pin': Pin to be read. Any pin from 1 to 16
+      Return: True if state == 1, False if state == 0
    """
    if(str(pin).isdigit() and (0 < pin < 17)):
       if(0 < pin < 9):
-         out = str(bin(i2c.read_byte_data(0x26, 0x13))).replace("0b", "").rjust(8, '0')[pin - 1]
+         out = str(bin(i2c.read_byte_data(ADDRESS, 0x13))).replace("0b", "").rjust(8, '0')[pin - 1]
       elif(8 < pin < 17):
-         out = str(bin(i2c.read_byte_data(0x26, 0x12))).replace("0b", "").rjust(8, '0')[pin - 9]
+         out = str(bin(i2c.read_byte_data(ADDRESS, 0x12))).replace("0b", "").rjust(8, '0')[pin - 9]
       return True if out == "1" else False
